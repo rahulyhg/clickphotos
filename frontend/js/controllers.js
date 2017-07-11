f3de1fb650702eb68f124bdbdc460c450057e1e7
NileshWangad <nilesh.wangad@wohlig.com>
2017-07-11 16:39:38 +0530

    Invoice Page and Featured Photographer Step3

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1369,6 +1369,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
             $rootScope.showStep = 1;
         }
         //console.log($scope.template);
+        $scope.states=["Andaman and Nicobar Islands","Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chandigarh","Chhattisgarh","Dadra and Nagar Haveli","Daman and Diu","Delhi","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Lakshadweep","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Orissa","Pondicherry","Punjab","Rajasthan","Sikkim","Tamil Nadu","Tripura","Uttar Pradesh","Uttaranchal","West Bengal"]
     })
 
     .controller('UsersCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
