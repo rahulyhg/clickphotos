39a0bbbbe96d09b772c9f8525cc7269b09522b0c
Bhargavpurohit <bhargav.purohit@wohlig.com>
2017-08-21 13:51:49 +0530

    passport login done

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,6 @@
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
-global["GoogleclientId"] = "529279279497-hdm2ul03erq4kitk7qlqbf41h6pl8f7p.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "nNZiqXW5U2364QI9--sVIR8B";
+global["GoogleclientId"] = "1094976383000-q56keu3n540djsbohe4hic7e60od43qa.apps.googleusercontent.com";
+global["GoogleclientSecret"] = "5LUtYOs_DdbBD5s6w0Qf1yqa";
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
